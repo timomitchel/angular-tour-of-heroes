da8434fc56e76b8d6c3101eca261513e5452e942
Tim Tyrrell <tim.tyrrell@crownpeak.com>
2018-05-11 16:00:42 -0600

    create list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mike Dao' },
+    { id: 12, name: 'Ellen Mary' },
+    { id: 13, name: 'Ian' },
+    { id: 14, name: 'Allison' },
+    { id: 15, name: 'Josh' },
+    { id: 16, name: 'Sal' },
+    { id: 17, name: 'Ilana' },
+    { id: 18, name: 'Victoria' },
+    { id: 19, name: 'Jeff' },
+    { id: 20, name: 'Erin' }
+];
