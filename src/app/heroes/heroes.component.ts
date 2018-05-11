54feb0b128911393b6b1d22521981d66ce480fa9
Tim Tyrrell <tim.tyrrell@crownpeak.com>
2018-05-11 16:00:15 -0600

    update heroes template and add onSelect event function

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{ Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,7 @@ import{ Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES
   hero: Hero = { 
     id: 1, 
     name: 'Ilana'
@@ -14,6 +16,12 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
